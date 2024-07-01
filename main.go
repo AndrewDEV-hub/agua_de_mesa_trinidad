6c0a9910235972270811c3914530a8b1c9789b02
AndrewDEV-hub <andrewahdev@gmail.com>
2024-07-01 04:13:08 -0400

    beta 11

--- main.go
+++ main.go
@@ -9,17 +9,19 @@ import (
 func main() {
     port := os.Getenv("PORT")
     if port == "" {
-        log.Fatal("PORT environment variable not set")
+        port = "8080" // Define un puerto por defecto si PORT no está configurado
+        log.Printf("Defaulting to port %s", port)
     }
 
-    // Ruta para servir el sitemap.xml
+    // Manejador para servir el sitemap.xml
     http.HandleFunc("/sitemap.xml", func(w http.ResponseWriter, r *http.Request) {
         http.ServeFile(w, r, "sitemap.xml")
     })
 
-    // Ruta por defecto para servir otros archivos o contenido
+    // Manejador para servir otros archivos estáticos desde el directorio ./src
     http.Handle("/", http.FileServer(http.Dir("./src")))
 
+    // Iniciar el servidor HTTP en el puerto especificado por la variable de entorno PORT
     log.Printf("Starting server on port %s", port)
     if err := http.ListenAndServe(":"+port, nil); err != nil {
         log.Fatal(err)
