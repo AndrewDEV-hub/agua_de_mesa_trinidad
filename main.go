ff9001230d8e5358c468d4b81ccc7fccced2576d
AndrewDEV-hub <andrewahdev@gmail.com>
2024-07-01 03:58:29 -0400

    beta 10

--- main.go
+++ main.go
@@ -1,25 +1,27 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
+    "log"
+    "net/http"
+    "os"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-	if port == "" {
-		log.Fatal("PORT environment variable not set")
-	}
+    port := os.Getenv("PORT")
+    if port == "" {
+        log.Fatal("PORT environment variable not set")
+    }
 
-	// Establece el directorio desde el cual se sirven los archivos estáticos
-	fs := http.FileServer(http.Dir("./src"))
+    // Ruta para servir el sitemap.xml
+    http.HandleFunc("/sitemap.xml", func(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, "sitemap.xml")
+    })
 
-	// Manejador para la raíz
-	http.Handle("/", fs)
+    // Ruta por defecto para servir otros archivos o contenido
+    http.Handle("/", http.FileServer(http.Dir("./src")))
 
-	log.Printf("Starting server on port %s", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal(err)
-	}
+    log.Printf("Starting server on port %s", port)
+    if err := http.ListenAndServe(":"+port, nil); err != nil {
+        log.Fatal(err)
+    }
 }
