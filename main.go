bb59b4a5dcbef2ff18d3a6b0c9fe53778a03421c
AndrewDEV-hub <andrewahdev@gmail.com>
2024-07-01 03:51:15 -0400

    beta 8

--- main.go
+++ main.go
@@ -1,21 +1,25 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "os"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
-    port := os.Getenv("PORT")
-    if port == "" {
-        log.Fatal("PORT environment variable not set")
-    }
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT environment variable not set")
+	}
 
-    http.Handle("/", http.FileServer(http.Dir("./src")))
+	// Establece el directorio desde el cual se sirven los archivos estáticos
+	fs := http.FileServer(http.Dir("./src"))
 
-    log.Printf("Starting server on port %s", port)
-    if err := http.ListenAndServe(":"+port, nil); err != nil {
-        log.Fatal(err)
-    }
+	// Manejador para la raíz
+	http.Handle("/", fs)
+
+	log.Printf("Starting server on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal(err)
+	}
 }
